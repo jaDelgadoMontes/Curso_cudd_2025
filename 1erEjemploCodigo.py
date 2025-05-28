d0d4ccb2ee22c375bbc171c3fcd474cc535e4f1a
JuanArmando DelgadoMontes <63225415+jaDelgadoMontes@users.noreply.github.com>
2025-05-28 16:49:01 -0700

    Update 1erEjemploCodigo.py

--- 1erEjemploCodigo.py
+++ 1erEjemploCodigo.py
@@ -1,2 +1,18 @@
+# mini bot
 import streamlit as st
-st.title("1er Ejemplo con Streamlit y Python")
+
+#st.set_page_config(page_title="Ejemplo Chat", layout="centered")
+
+st.title("💬 Mini Chatbot (solo repite lo que dices)")
+
+# Entrada tipo chat (abajo de la pantalla)
+user_input = st.chat_input("Escribe algo...")
+
+# Si el usuario escribe algo, mostramos los mensajes
+if user_input:
+    # Mostrar el mensaje del usuario
+    st.chat_message("user").write(user_input)
+
+    # Mostrar una respuesta simple del asistente
+    st.chat_message("assistant").write(f"{user_input} <- eso dijiste")
+
