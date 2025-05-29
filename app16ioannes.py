94a9d938552ca549b26eda603c6be05dc4e284d8
JuanArmando DelgadoMontes <63225415+jaDelgadoMontes@users.noreply.github.com>
2025-05-29 15:08:25 -0700

    Update app16ioannes.py

--- app16ioannes.py
+++ app16ioannes.py
@@ -12,7 +12,7 @@ prompt = st.chat_input("Ingresa tu consulta")
 if prompt==None:
    st.stop()
 
-with st.chat_message("user"):
+with st.chat_message("user", avatar = "👀"):
    st.markdown(prompt)
 
 # Generate a response using the OpenAI API.
